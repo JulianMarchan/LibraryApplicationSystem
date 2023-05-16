3adeaebc4bb15d8b29f6201acf84bfefbadb75ca
DBASYSTEM\Julian.Marchan <julian.marchan@dbaadvisory.com>
2023-05-16 10:54:59 +0800

    books

---
+++ src/LibraryApplicationSystem.Web.Mvc/wwwroot/view-resources/Views/Books/CreateOrEditBook.js
@@ -0,0 +1,34 @@
+﻿(function ($) {
+
+    var _$form = $('#CreateBookForm');
+    var _bookCategoriesAppService = abp.services.app.Books;
+    var _indexPage = "/Books";
+
+    _$form.submit(function (e) {
+        e.preventDefault();
+        save();
+    });
+
+    function save() {
+        if (!_$form.valid()) {
+            return;
+        }
+        var book = _$form.serializeFormToObject();
+
+        if (book.Id != 0) {
+            _bookCategoriesAppService.update(book).done(function () {
+                window.location.href = _indexPage;
+            });
+        } else {
+            _bookCategoriesAppService.create(book).done(function () {
+                window.location.href = _indexPage;
+            });
+        }
+    }
+    $(document).on('click', '.cancel-button', function (e) {
+        window.location.href = _indexPage;
+    });
+
+
+
+})(jQuery);
---
+++ src/LibraryApplicationSystem.Web.Mvc/wwwroot/view-resources/Views/Books/Index.js
@@ -0,0 +1,42 @@
+﻿(function ($) {
+    l = abp.localization.getSource('LibraryApplicationSystem');
+    var _bookAppService = abp.services.app.Books; //APP SERVICE
+
+    // EDIT UPDATE
+    $(document).on('click', '.edit-dept', function (e) {
+        var deptId = $(this).attr("data-dept-id");
+
+        e.preventDefault();
+        window.location.href = "/BooksCategories/CreateOrEditBook/" + deptId;
+    });
+
+    $(document).on('click', '.delete-dept', function () {
+        var deptId = $(this).attr("data-dept-id");
+        var deptName = $(this).attr('data-user-name');
+
+        deleteDept(deptId, deptName);
+    });
+
+    function deleteDept(deptId, deptName) {
+        abp.message.confirm(
+            abp.utils.formatString(
+                l('AreYouSureWantToDelete'),
+                deptName),
+            null,
+            (isConfirmed) => {
+                if (isConfirmed) {
+                    _bookAppService.delete({
+                        id: deptId
+                    }).done(() => {
+                        abp.notify.info(l('SuccessfullyDeleted'));
+                        window.location.href = "/Books";
+                        //_$form.ajax.reload();
+                    });
+                }
+            }
+        );
+    }
+
+
+})(jQuery);
+
